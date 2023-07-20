c9f000c33c8459634ea0052eaca5cbd079833268
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-07-20 10:44:26 +0200

    Code carrousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,50 +3,46 @@ import "../styles/carrousel.css";
 import { useState } from "react";
 
 const Carrousel = ({ objet }) => {
-    console.log(objet[0].cover)
+  console.log(objet[0].cover);
 
-    const covers = objet.map((objet) => objet.cover);
-    console.log(covers)
+  const covers = objet.map((objet) => objet.cover);
+  console.log(covers);
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndexA, setCurrentIndexA] = useState(covers.length - 1);
+  const [currentIndexB, setCurrentIndexB] = useState(1);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const isFirstSlideA = currentIndexA === 0;
+    const isFirstSlideB = currentIndexB === 0;
+    const newIndex = isFirstSlide ? covers.length - 1 : currentIndex - 1;
+    const newIndexA = isFirstSlideA ? covers.length - 1 : currentIndexA - 1;
+    const newIndexB = isFirstSlideB ? covers.length - 1 : currentIndexB - 1;
+    setCurrentIndex(newIndex);
+    setCurrentIndexA(newIndexA);
+    setCurrentIndexB(newIndexB);
 
-  
+  };
 
-    const goToPrevious = () => {
-        const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? covers.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
-      };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === covers.length - 1;
+    const isLastSlideA = currentIndexA === covers.length - 1;
+    const isLastSlideB = currentIndexB === covers.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    const newIndexA = isLastSlideA ? 0 : currentIndexA + 1;
+    const newIndexB = isLastSlideB ? 0 : currentIndexB + 1;
+    setCurrentIndex(newIndex);
+    setCurrentIndexA(newIndexA);
+    setCurrentIndexB(newIndexB);
     
-      const goToNext = () => {
-        const isLastSlide = currentIndex === covers.length - 1;
-        const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
-      };
+  };
 
-      return (
-        <div className="carrousel">
-
-
-
-<div
+  return (
+    <div className="carrousel">
+      <div
         className="slideStyles"
-        style={{ backgroundImage: `url(${covers[currentIndex]})` }}
-      >
-        {covers.length !== 1 && (
-          <React.Fragment>
-            
-          <div className="fleches">
-              <img onClick={goToPrevious} src ="../fleche-left.png" alt="left-arrow"/>
-         
-              <img onClick={goToNext} src ="../fleche-right.png" alt="right-arrow"/>
-              </div>
-          </React.Fragment>
-        )}
-      </div>
-
-
-
+        style={{ backgroundImage: `url(${covers[currentIndexA]})` }}
+      ></div>
 
       <div
         className="slideStyles"
@@ -54,21 +50,36 @@ const Carrousel = ({ objet }) => {
       >
         {covers.length !== 1 && (
           <React.Fragment>
-            
-          <div className="fleches">
-              <img onClick={goToPrevious} src ="../fleche-left.png" alt="left-arrow"/>
-         
-              <img onClick={goToNext} src ="../fleche-right.png" alt="right-arrow"/>
-              </div>
-              <div className="numerotation">
-        <p>{currentIndex + 1}/{covers.length}</p>
-      </div>
+            <div className="fleches">
+              <img
+                onClick={goToPrevious}
+                src="../fleche-left.png"
+                alt="left-arrow"
+              />
+
+              <img
+                onClick={goToNext}
+                src="../fleche-right.png"
+                alt="right-arrow"
+              />
+            </div>
+            <div className="numerotation">
+              <p>
+                {currentIndex + 1}/{covers.length}
+              </p>
+            </div>
           </React.Fragment>
         )}
       </div>
+
+      <div
+        className="slideStyles"
+        style={{ backgroundImage: `url(${covers[currentIndexB]})` }}
+      ></div>
+
+
     </div>
-      )
+  );
+};
 
-}
-
-export default Carrousel
+export default Carrousel;
