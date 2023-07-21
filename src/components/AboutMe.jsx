f237f2028727a7b7fc432ce47743c7bdf3b98dd1
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-07-21 14:03:05 +0200

    mep_icones

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React from "react";
 import "../styles/aboutMe.css";
 
-
 const AboutMe = () => {
-    return (
-        <section className="aboutMe" id="about">
-          <div className="a_propos">
-            <h1>A propos de moi...</h1>
-            <p>Bonjour aujourd'hui je vais faire un blabla très court pour parler de moi et de mes compétences en développement<br />
-           Aussi, je suis très intéressé pour rendre vos projets esthétiques et blbsdbfbvpfvfvbpbpbdbvb</p>
-           <div className="liens">
-           <a href="https://www.facebook.com/openclassroomsfr">
-  <img src="icons8-facebook-nouveau.svg" alt="Icône Facebook"></img>
-</a>
-<a href="https://www.instagram.com/openclassrooms/r">
-  <img src="icons8-instagram.svg" alt="Icône Facebook"></img>
-</a>
-<a href="https://github.com/alexhcrel?tab=repositories">
-  <img src="icons8-github.svg" alt="Icône Facebook"></img>
-</a>
-
-           </div>
-          </div>
-        </section>
-    );
+  return (
+    <section className="aboutMe" id="about">
+      <div className="a_propos">
+        <h1>A propos de moi...</h1>
+        <p>
+          Bonjour aujourd'hui je vais faire un blabla très court pour parler de
+          moi et de mes compétences en développement
+          <br />
+          Aussi, je suis très intéressé pour rendre vos projets esthétiques et
+          blbsdbfbvpfvfvbpbpbdbvb
+        </p>
+        <div className="liens">
+          <a href="https://www.facebook.com/openclassroomsfr">
+            <img src="icons8-facebook-nouveau.svg" alt="Icône Facebook"></img>
+          </a>
+          <a href="https://www.instagram.com/openclassrooms/r">
+            <img src="icons8-instagram.svg" alt="Icône Facebook"></img>
+          </a>
+          <a href="https://github.com/alexhcrel?tab=repositories">
+            <img src="icons8-github.svg" alt="Icône Facebook"></img>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutMe;
