7a3dafa4e31912c81752f764db495e1f3221ba6e
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-08-10 11:00:47 +0200

    correction_sophie

--- src/formulaire.js
+++ src/formulaire.js
@@ -1,16 +1,3 @@
-// // formulaire.js
-
-// export function addFormEventListener() {
-//   const formulaire = document.getElementById("formulaire")
-//   formulaire.addEventListener("submit", function () {
-//     const alert = document.querySelector(".alert");
-//     const suppression = document.createElement("button");
-//     alert.appendChild(suppression)
-
-//     alert.style.opacity = "1"
-
-//  });
-//   };
 
 export function addFormEventListener() {
   const formulaire = document.getElementById("formulaire");
@@ -40,42 +27,3 @@ export function addFormEventListener() {
     }
   });
 }
-
-//  export function closeAlert() {
-//   const alert = document.querySelector(".alert")
-//   if (alert.style.opacity === "1") {
-//     const boutonSuppression = document.querySelector(".boutonSuppression")
-//     boutonSuppression.addEventListener("click", function () {
-//       console.log("pouet")})
-//     }
-//     }
-
-//   formulaire.addEventListener("submit", function (event) {
-
-//     suppression.className = "boutonSuppression";;
-//     suppression.innerText = "OK"
-// );
-//   })
-// }
-
-//   const alert = document.querySelector(".alert");
-//   console.log(alert)
-//     const suppression = document.createElement("button")
-//     suppression.className = "boutonSuppression";;
-//     suppression.innerText = "OK"
-//     alert.appendChild(suppression);
-//     console.log(suppression)
-
-// }
-
-// function closeCustomAlert(event) {
-//   console.log("pouet")
-//   //
-//   // console.log(customAlert)
-//   // customAlert.style.display = "hidden";
-// }
-// closeCustomAlert()
-
-// suppression.type = "submit"
-// suppression.onclick="closeCustomAlert()"
-// alert.appendChild(suppression)
